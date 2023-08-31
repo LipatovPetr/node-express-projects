f1770a9187750de9daca0a5aab32c6bca0523e59
Petr Lipatov <lipatov3@gmail.com>
2023-09-01 01:19:14 +0300

    feat: server init with err handling

--- store-api/app.js
+++ store-api/app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware
+
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+//
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3030;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Sever is listening in port ${port}`));
+  } catch (err) {}
+};
+
+start();
