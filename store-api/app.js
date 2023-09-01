6e9b22f956ab4e2d4933a41ea0e2c26f7a9129cb
Petr Lipatov <lipatov3@gmail.com>
2023-09-01 15:41:03 +0300

    feat: data model

--- store-api/app.js
+++ store-api/app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
+
 const connectDB = require("./db/connect");
 const productsRouter = require("./routes/products");
 
