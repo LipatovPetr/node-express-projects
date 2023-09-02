01da56a72f1a35634c705372281e11276ddd4fb5
Petr Lipatov <lipatov3@gmail.com>
2023-09-03 01:12:14 +0300

    refactor: auth check moved to middleware

--- jwt-basics/controllers/main.js
+++ jwt-basics/controllers/main.js
@@ -11,30 +11,15 @@ const login = async (req, res) => {
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-
-  console.log(username, password);
   res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomAPIError("No token provided", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const randomNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your secrete number ${randomNumber}`,
-    });
-  } catch {
-    throw new CustomAPIError("Not authorized to access the route", 401);
-  }
+  const randomNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your secrete number ${randomNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
