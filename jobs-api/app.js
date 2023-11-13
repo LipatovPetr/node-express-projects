61d99ae543e98c9d55a91d72545719223a5ed8ea
Petr Lipatov <lipatov3@gmail.com>
2023-11-13 18:53:34 +0300

    feat: express json middleware added, createUser controller added

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
-// connectDB
-
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
+
+// extra security packages
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // routers
 
 const authRouter = require("./routes/auth");
@@ -16,8 +23,12 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(rateLimiter({ windiwMs: 15 * 60 * 1000, max: 100 }));
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 
